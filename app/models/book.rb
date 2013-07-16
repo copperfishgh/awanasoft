6c73c45dd21c76cd3f6549caeda9bdc6dfdd7cb2
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-07-15 22:03:27 -0700

    fixed validation message to include all allowed values

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,5 +4,5 @@ class Book < ActiveRecord::Base
   validates_presence_of :name, :club
   validates_format_of :club,
     :with => /^(cubbies|sparks|t\&t|workers|puggles|trek)$/,
-    :message => "club must be one of: 'cubbies', 'sparks', 't&t', or 'workers'" 
+    :message => "club must be one of: 'puggles', 'cubbies', 'sparks', 't&t', 'trek', or 'workers'" 
 end
